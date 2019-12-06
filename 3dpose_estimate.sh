e73742154cb2b03fd0d4fff7adb9542ad490d425
Savin Vadim <savinvadim1312@gmail.com>
2019-12-06 13:58:37 +0100

    Update 3dpose_estimate.sh

--- 3dpose_estimate.sh
+++ 3dpose_estimate.sh
@@ -9,7 +9,7 @@ for dir in keras_Realtime_Multi-Person_Pose_Estimation/sample_images/*; do
 	  echo "Processing $no_ext from $dir"
 
 	  python2 hmr/demo.py --img_path $f \
-			     --json_path keras_Realtime_Multi-Person_Pose_Estimation/sample_jsons/$dir/$no_ext.json  
+			     --json_path $dir/$no_ext.json  
 
 	done
 done
