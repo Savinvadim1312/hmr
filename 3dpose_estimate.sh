c2cfd7b380eb3d037df1c22b6db88d1a699dea81
Savin Vadim <savinvadim1312@gmail.com>
2019-12-06 14:28:59 +0100

    Update 3dpose_estimate.sh

--- 3dpose_estimate.sh
+++ 3dpose_estimate.sh
@@ -3,13 +3,14 @@
 for dir in keras_Realtime_Multi-Person_Pose_Estimation/sample_images/*; do
 	for f in $dir/*; do
 
-		filename=$(basename -- "$f")
+	  videoname=$(basename -- "$dir")
+	  filename=$(basename -- "$f")
 	  no_ext="${filename%.*}"
 
-	  echo "Processing $no_ext from $dir"
+	  echo "Processingi video: $videoname image: $no_ext from $dir"
 
 	  python2 hmr/demo.py --img_path $f \
-			     --json_path $dir/$no_ext.json  
+			     --json_path keras_Realtime_Multi-Person_Pose_Estimation/sample_jsons/$videoname/$no_ext.json  
 
 	done
 done
